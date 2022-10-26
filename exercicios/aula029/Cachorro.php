98a7c49147d04799c972b324488adc6531182acb
Diego Henrique <D.HenriqueXVII@Gmail.com>
2022-10-26 06:50:26 -0300

    Novos exercícios adicionados

--- exercicios/aula029/Cachorro.php
+++ exercicios/aula029/Cachorro.php
@@ -1,5 +1,17 @@
 <?php
 
-require_once 'Mamifero';
+require_once 'Mamifero.php';
 
-class Cachorro extends Mamifero {}
+class Cachorro extends Mamifero {
+    public function emitirSom() {
+        echo "Auau";
+    }
+    
+    public function enterrarOsso() {
+        echo "Enterrando osso";
+    }
+
+    public function abanarRabo() {
+        echo "Abanando o rabo";
+    }
+}
--- exercicios/aula029/Canguru.php
+++ exercicios/aula029/Canguru.php
@@ -2,4 +2,12 @@
 
 require_once 'Mamifero.php';
 
-class Canguru extends Mamifero {}
+class Canguru extends Mamifero {
+    public function usarBolsa() {
+        echo 'Usando bolsa';
+    }
+
+    public function locomover() {
+        echo "Pulando";
+    }
+}
